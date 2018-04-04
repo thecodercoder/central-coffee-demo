3ee0940489b75d21b6fb8602e47a3a7fb3fa8448
Jessica Oei <wemaycode@users.noreply.github.com>
2018-04-03 22:31:06 -0500

    Organize SCSS, add styles for Home and About sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 // Initialize packges used in this Gulpfile
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var wait = require('gulp-wait');
 
 // Sass task: compiles the style.scss file into style.css in the same directory
 gulp.task('sass', function(){
     return gulp.src('app/scss/style.scss')
+        .pipe(wait(500))
         .pipe(sass())
         .pipe(gulp.dest('dist'))
 });
 
-// Watch task: watches for changes to style.scss, then runs the 'sass' task
+// Watch task: watches for changes to *.scss, then runs the 'sass' task
 gulp.task('watch', function(){
-    gulp.watch('style.scss', ['sass']);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
 // Defines the default task to run, when you type in "gulp" into the command line.
